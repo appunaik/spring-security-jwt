0563d76045cb65e3c2acea3ea9f38da863edf8a0
appunaik <appunaik@yahoo.com>
2024-10-03 15:44:34 +0530

    updated a file

--- src/main/java/com/security/jwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/security/jwt/config/JwtAuthenticationFilter.java
@@ -37,6 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
       filterChain.doFilter(request, response);
       return;
     }
+
     final String authHeader = request.getHeader("Authorization");
     final String jwt;
     final String userEmail;
