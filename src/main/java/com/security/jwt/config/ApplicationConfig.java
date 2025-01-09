0a4519203220b37e4bf51fa22b893e6af5b69297
appunaik <appunaik@yahoo.com>
2025-01-09 16:51:34 +0530

    Spring securty with JWT

--- src/main/java/com/security/jwt/config/ApplicationConfig.java
+++ src/main/java/com/security/jwt/config/ApplicationConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +29,14 @@ public class ApplicationConfig {
     UserDetailsService userDetailsService() {
     return username -> repository.findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+//    	return new UserDetailsService() {
+//			
+//			@Override
+//			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//				// TODO Auto-generated method stub
+//				return null;
+//			}
+//		};
   }
 
     @Bean
